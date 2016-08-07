b86e108b64920a193b73c10d95c558de9ba3d703
KechuNet <nicolaskinaschuk@gmail.com>
2016-08-07 12:22:14 -0300

    Se agrego output a las respuestas

--- mocManagerController.py
+++ mocManagerController.py
@@ -22,18 +22,23 @@ class MocManagerController(object):
 		result = ml.getMp3s()
 		req.context['result'] = result
 
+
+
 		resp.set_header('Powered-By', 'Falcon')
 		resp.status = falcon.HTTP_200
 
-	#@falcon.before(max_body(64 * 1024))
 	def on_post(self, req, resp, user_id):
 		try:
 			request = req.context['doc']
 			action = int(request['Action'])
-			mp3Path = int(request['Mp3'])
-			self.actions[action](mp3Path)
+
+			if not action in range(1,5):
+				req.context['result'] = "Invalid Operation"
+			else:
+				mp3Path = int(request['Mp3'])
+				self.actions[action](mp3Path)
+
 		except Exception as ex:
-			print(ex)
 			self.logger.error(ex)
 
 		resp.set_header('Powered-By', 'Falcon')
