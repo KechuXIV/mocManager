ff79bb63154f97af00aa48912cd6d0c0fb119aab
Nicolas Kinaschuk <nkinaschuk@zoologicnet.com.ar>
2016-08-22 16:37:12 -0300

    Se actualizo .gitignore y se agrego trace a mocManagerController

--- mocManagerController.py
+++ mocManagerController.py
@@ -19,27 +19,31 @@ class MocManagerController(object):
 		}
 
 	def on_get(self, req, resp, user_id):
+		print("Entro Por Get")
 		result = ml.getMp3s()
 		req.context['result'] = result
-
-
 
 		resp.set_header('Powered-By', 'Falcon')
 		resp.status = falcon.HTTP_200
 
 	def on_post(self, req, resp, user_id):
 		try:
+			print("Entro Por post")
 			request = req.context['doc']
+			print("Request es {0}".format(request))
 			action = int(request['Action'])
+			print("Action es {0}".format(action))
 
 			if not action in range(1,5):
 				req.context['result'] = "Invalid Operation"
 			else:
 				mp3Path = int(request['Mp3'])
+				print("mp3Path es {0}".format(mp3Path))
 				self.actions[action](mp3Path)
 
 		except Exception as ex:
-			self.logger.error(ex)
+			#self.logger.error(ex)
+			raise ex
 
 		resp.set_header('Powered-By', 'Falcon')
 		resp.status = falcon.HTTP_200
